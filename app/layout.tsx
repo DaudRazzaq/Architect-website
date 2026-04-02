ed0753d4f4425dc9d0bb8c1bf1ba8ab810482693
Muhammad Daud Razzaq Khan <139013552+DaudRazzaq@users.noreply.github.com>
2026-04-02 10:30:07 +0500

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Preloader from "./components/Preloader";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
